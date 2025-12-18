54baf4a069387e53c72415280f8f0b759b147660
Parwez Alam <parwezalamqa@gmail.com>
2025-12-18 13:27:14 +0530

    Google2

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -6,4 +6,5 @@ test('My First Test', async ({ page }) => {
 
     await page.goto('https://google.com')
     await expect(page).toHaveTitle('Google')
+    await expect(page).toHaveTitle('Google2')
 })
